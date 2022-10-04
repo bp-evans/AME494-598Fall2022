0dbbee2e671622db1316738e7f24afe45c8464f5
TheWumbologist <bpevans@asu.edu>
2022-10-04 12:56:46 -0700

    Changes

--- A4/code/ws-server/server.js
+++ A4/code/ws-server/server.js
@@ -6,9 +6,10 @@ const WebSocket = require('ws')
 
 
 const wss = new WebSocket.Server({ port: 3000})
-
+var count = 0;
 wss.on('connection', ws => {
   ws.on('message', message => {
+    console.log(count, message, new Date());count++;
 	var x = `${message}`;
 	  console.log(x, new Date());
   })
